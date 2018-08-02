3823cd424b8c70e4d4cea582bb0afa4c659381a4
Trevor Meadows <tlm04070@gmail.com>
2018-08-02 18:58:38 -0400

    initial file structure setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
   module: {
     rules: [
@@ -5,9 +12,26 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "bablel-loader"
+          loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
-  }
+  },
+  plugins: [htmlPlugin]
 };
