56bfeaea65e070851c8575c88e164a09052f1caf
james <jwin4740@gmail.com>
2018-08-09 19:57:57 -0400

    team rendering

--- src/components/pages/create-team-page/create-team-page-main.js
+++ src/components/pages/create-team-page/create-team-page-main.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import CreateTeamSocialMedia from "./social-media/create-team-page-social-media";
-import TitleAndBox from "../../global/title-and-box";
 import CreateTeamPageSocialMedia from "./social-media/create-team-page-social-media";
 
 class CreateTeamPageMain extends Component {
   render() {
     return (
       <div>
-        <TitleAndBox title="Social" width="25%">
+      
           <CreateTeamPageSocialMedia />
-        </TitleAndBox>
+      
       </div>
     );
   }
--- src/components/pages/create-team-page/social-media/create-team-page-social-media.js
+++ src/components/pages/create-team-page/social-media/create-team-page-social-media.js
@@ -1,23 +1,9 @@
 import React, { Component } from "react";
 
 class CreateTeamPageSocialMedia extends Component {
-  createInputs() {
-    const socialInputs = ["twitter", "instagram", "youtube", "stream"];
-    return socialInputs.map((val, i) => {
-      return (
-        <LabelInput
-          key={i}
-          name={val}
-          label={val}
-          className="socialMediaInput"
-          placeholder={val + " name"}
-        />
-      );
-    });
-  }
 
   render() {
-    return <div>{this.createInputs()}</div>;
+    return <div>hello</div>;
   }
 }
 
