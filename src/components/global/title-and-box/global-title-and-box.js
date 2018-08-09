0607a95049bf213ad9c159a481ecdf16ca552f47
james <jwin4740@gmail.com>
2018-08-08 22:03:24 -0400

    practive shell

---
+++ src/components/global/title-and-box/global-title-and-box.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Link } from "react-router";
+import Button from "../buttons";
+
+class TitleAndBox extends Component {
+  render(props) {
+    return (
+      <div className="title-and-box">
+ 
+      </div>
+    );
+  }
+}
+
+export default Navbar;
