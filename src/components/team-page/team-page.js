18006ba15e2fbb171c7d2d351f7ad528fa85dbd3
Dale Holstein <holstein30@live.com>
2018-08-08 22:02:02 -0400

    header started

--- src/components/team-page/team-page.js
+++ src/components/team-page/team-page.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Avatar from "../global/avatar";
 
 class TeamPage extends Component {
   render() {
-    return <div>Testing Team Page</div>;
+    return (
+      <div>
+        <div className="header-container">
+          <Avatar avatar="../../../public/images/ph1.jpg" />
+          <h1>username</h1>
+        </div>
+      </div>
+    );
   }
 }
 
