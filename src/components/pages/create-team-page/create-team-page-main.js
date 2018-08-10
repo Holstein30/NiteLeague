5cb14ad7b74a35f7d4fe0807a35a12cf42165e15
james <jwin4740@gmail.com>
2018-08-09 20:44:14 -0400

    team logo avatar good

--- src/components/pages/create-team-page/create-team-page-main.js
+++ src/components/pages/create-team-page/create-team-page-main.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import CreateTeamPageSocialMedia from "./social-media/create-team-page-social-media";
 import CreateTeamPageTeamName from "./team-name/create-team-page-team-name";
+import { CreateTeamPageTeamLogo } from "./team-logo";
 class CreateTeamPageMain extends Component {
   render() {
     return (
       <div>
       <CreateTeamPageTeamName />
+      <CreateTeamPageTeamLogo />
           <CreateTeamPageSocialMedia />
       
       </div>
---
+++ src/components/pages/create-team-page/team-logo/index.js
@@ -0,0 +1,2 @@
+import CreateTeamPageTeamLogo from "./create-team-page-team-logo";
+export {CreateTeamPageTeamLogo};
