94c80de16c9b07baba080086ebd6a58fe407f5dc
Dale Holstein <holstein30@live.com>
2018-08-15 18:22:46 -0400

    click event function firing

--- src/components/pages/home-page/create/home-page-create.js
+++ src/components/pages/home-page/create/home-page-create.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Button from "../../../global/buttons";
 
 class HomePageCreate extends Component {
+  handleEventCreateLeague() {
+    console.log("click");
+  }
+
   render() {
     return (
       <div>
@@ -9,7 +13,11 @@ class HomePageCreate extends Component {
           src="../../../../../public/images/ph1.jpg"
           className="create-league-img"
         />
-        <Button className="create-league-btn" name="Create League" />
+        <Button
+          className="create-league-btn"
+          name="Create League"
+          handleClick={this.handleEventCreateLeague}
+        />
         <h3>How It Works</h3>
         <p className="how-it-works-p">
           Lorem ipsum dolor amet art party truffaut cold-pressed, try-hard pok
