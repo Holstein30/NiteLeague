e49de1ec3d629e594c0839bba8c59cc64e4211ac
Dale Holstein <holstein30@live.com>
2018-08-14 22:05:25 -0400

    comments for homepage functionality todos

--- src/components/pages/home-page/home-page.js
+++ src/components/pages/home-page/home-page.js
@@ -15,3 +15,10 @@ class HomePage extends Component {
 }
 
 export default HomePage;
+
+// * Functionality *
+// ! Make sure there are League's in DB
+// TODO: Grab League's from DB to be featured
+// TODO: Authentication flow working (display username when logged in - display login/registration if not)
+// TODO: Event handler for profile/login/registration
+// TODO: Event handler for create/join a league buttons
