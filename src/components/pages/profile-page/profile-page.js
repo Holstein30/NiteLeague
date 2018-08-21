8e5d220d84ec1c5a75bc8a7bc39764d8077a3f3e
Trevor Meadows <tlm04070@gmail.com>
2018-08-21 18:28:25 -0400

    added axios to test crud to db

--- src/components/pages/profile-page/profile-page.js
+++ src/components/pages/profile-page/profile-page.js
@@ -4,13 +4,38 @@ import ProfilePageLinks from "./links";
 import CurrentTeam from "./list/profile-page-list-teams";
 import CurrentLeague from "./list/profile-page-list-leagues";
 import Button from "../../global/buttons";
+
+import axios from "axios";
 class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://192.168.1.2:8080/api/users/").then(res => {
+      const user = res.data;
+      this.setState({
+        user: user
+      });
+      console.log(this.state.user);
+    });
+  }
   render() {
     return (
       <div>
         <div className="header-container" style={{ border: "solid 2px" }}>
           <Avatar avatar="../../../../../public/images/ph1.jpg" />
-          <h1>this.state.username</h1>
+          {this.state.user.map(person => {
+            return (
+              <div>
+                <h1>Name: {person.name}</h1>
+                <h1>Username: {person.username}</h1>
+              </div>
+            );
+          })}
           <ProfilePageLinks />
         </div>
         <CurrentTeam />
